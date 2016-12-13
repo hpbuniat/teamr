0a93ed5d5a92602888b46337a859eddbbb96a1ab
Hans-Peter Buniat <hpbuniat@gmail.com>
2016-12-13 21:18:37 +0100

    remove freebsd

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
-for OS in "freebsd" "linux" "darwin" "windows"; do
+for OS in "linux" "darwin" "windows"; do
   for ARCH in "386" "amd64"; do
     VERSION="$(git describe --tags $1)"
     GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -ldflags "-X main.Version=$VERSION" -o teamr
